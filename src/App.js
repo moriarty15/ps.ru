ac742d56b870d2972c771aeea0291096e1fbf49f
Vladimir Palamov <84524771+moriarty15@users.noreply.github.com>
2023-02-17 17:01:53 +0300

    hello ps.ru

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Hello <span className='PS'>PS</span> <span className='RU'>.RU</span> </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
